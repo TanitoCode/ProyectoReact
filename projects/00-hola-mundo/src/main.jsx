7d87c91bda37b1411f59eaaad2309bce4f475d42
Mario Cristian Sanchez <mariocrsanchez@gmail.com>
2024-03-03 22:43:20 -0300

    Utilizamos React.Fragment

--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -4,5 +4,9 @@ import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  "Hola mundo"
+  <React.Fragment>
+<button data-id="123"> Button 1 </button>
+<button data-id="456"> Button 2 </button>
+<button data-id="789"> Button 3 </button>
+</React.Fragment>
 )
