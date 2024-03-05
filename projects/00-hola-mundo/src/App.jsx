f66a42e52ee0d91bb1c6a530fd327363a47ff5e4
Mario Cristian Sanchez <mariocrsanchez@gmail.com>
2024-03-04 22:27:39 -0300

    Creando componentes de las card

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,25 +1,13 @@
 import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App() {
     return (
-        <article className='tw-followCard'>
-            <header className='tw-followCard-header'>
-                <img
-                    className='tw-followCard-avatar'
-                    alt="El avatar de midudev"
-                    src="https://unavatar.io/midudev" />
-                <div className='tw-followCard-info'>
-                    <strong>
-                        Miguel Angel Durán
-                    </strong>
-                    <span className='tw-followCard-infoUserName'>@midudev</span>
-                </div>
-            </header>
-            <aside>
-                <button className='tw-followCard-button'>
-                    Seguir
-                </button>
-            </aside>
-        </article>
+        <section className='App'>
+            <TwitterFollowCard userName="midudev" name="Miguel Angel Durán" />
+            <TwitterFollowCard userName="pheralb" name="Pablo Hernandez" />
+            <TwitterFollowCard userName="elonmusk" name="Elon Musk" />
+            <TwitterFollowCard userName="vxnder" name="Vanderhart" />
+        </section>
     )
 }
