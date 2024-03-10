aa2194ac4a023656c77ad9b104dd2224ec8b07cd
Mario Cristian Sanchez <mariocrsanchez@gmail.com>
2024-03-10 18:53:34 -0300

    Actualizando el tablero al hacer click

--- projects/02-ta-te-ti/src/App.jsx
+++ projects/02-ta-te-ti/src/App.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react"
 
 const TURNS = {
   X: 'X',
-  O: 'o'
+  O: 'O'
 }
 
 
 
-const Square = ({ children, updateBoard, index}) => {
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard()
+  }
+
   return (
-    <div className="square">
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
@@ -19,6 +26,9 @@ function App() {
 
   const [turn, setTurn] = useState(TURNS.X)
 
+  const updateBoard = () => {
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+  }
 
   return (
     <main className='board'>
@@ -28,14 +38,24 @@ function App() {
           board.map((_, index) => {
             return (
               <Square
-              key={index}
-              index={index}
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
               >
-                  {index}
-                </Square>
+                {board[index]}
+              </Square>
             )
           })
         }
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
       </section>
     </main>
   )
