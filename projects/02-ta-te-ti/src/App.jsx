06b26a92dd16e2c18569aa0ed3ad26c04bed3a19
Mario Cristian Sanchez <mariocrsanchez@gmail.com>
2024-03-11 22:27:15 -0300

    Persistir partida en el localstorage

--- projects/02-ta-te-ti/src/App.jsx
+++ projects/02-ta-te-ti/src/App.jsx
@@ -7,9 +7,17 @@ import { checkWinnerFrom, checkEndGame} from "./logic/board"
 import { WinnerModal } from "./Components/WinnerModal"
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) :  Array(9).fill(null)
+  })
 
-  const [turn, setTurn] = useState(TURNS.X)
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+    TURNS.X
+  
+  })
   //null es que no hay ganador, false es que hay un empate
   const [winner, setWinner] = useState(null)
 
@@ -19,6 +27,9 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
   
@@ -40,7 +51,9 @@ function App() {
     //Cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
-
+    //Guardar partida
+    window.localStorage.setItem('board',JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
     //revisar si hay un ganador
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
